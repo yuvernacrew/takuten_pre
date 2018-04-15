bdc07bd852925f9c184e097e010c122a6fb16176
加藤　優奈 <yuvernacrew@gmail.com>
2018-04-15 22:14:09 +0900

    update: レイアウトを改修

--- js/script.js
+++ js/script.js
@@ -5,25 +5,108 @@ $(function(){
   var group = [
     {
       name : '卓1',
-      imgUrl: `${DIR_IMAGE}/taku_logo.jpg`,
+      imgUrl: `${DIR_IMAGE}/taku_logo.png`,
       slide: "https://www.slideshare.net/slideshow/embed_code/key/rhIYkBYQFRWEKm",
-      url: "https://www.slideshare.net/ItoYuki2/2017-75427213",
-      disc : `卓1ですこんにちはこんにちは`
+      disc : `卓1です</br>
+      こんにちは<b>こんにちは</b>
+      こんにちはこんにちは`
     },
     {
       name : '卓2',
-      imgUrl: `${DIR_IMAGE}/taku_logo.jpg`,
+      imgUrl: `${DIR_IMAGE}/taku_logo.png`,
       slide: "https://www.slideshare.net/slideshow/embed_code/key/rhIYkBYQFRWEKm",
-      url: "https://www.slideshare.net/ItoYuki2/2017-75427213",
-      disc : `卓2ですこんにちはこんにちは`
+      disc : `卓2です</br>
+      こんにちは<b>こんにちは</b>
+      こんにちはこんにちは`
     },
     {
       name : '卓3',
-      imgUrl: `${DIR_IMAGE}/taku_logo.jpg`,
+      imgUrl: `${DIR_IMAGE}/taku_logo.png`,
       slide: "https://www.slideshare.net/slideshow/embed_code/key/rhIYkBYQFRWEKm",
-      url: "https://www.slideshare.net/ItoYuki2/2017-75427213",
-      disc : `卓3ですこんにちはこんにちは`
+      disc : `卓3ですこんにちは</br>
+      こんにちは<b>こんにちは</b>
+      こんにちはこんにちは`
     },
+    {
+      name : '卓4',
+      imgUrl: `${DIR_IMAGE}/taku_logo.png`,
+      slide: "https://www.slideshare.net/slideshow/embed_code/key/rhIYkBYQFRWEKm",
+      disc : `卓4ですこんにちは</br>
+      こんにちは<b>こんにちは</b>
+      こんにちはこんにちは`
+    },
+    {
+      name : '卓5',
+      imgUrl: `${DIR_IMAGE}/taku_logo.png`,
+      slide: "https://www.slideshare.net/slideshow/embed_code/key/rhIYkBYQFRWEKm",
+      disc : `卓5ですこんにちは</br>
+      こんにちは<b>こんにちは</b>
+      こんにちはこんにちは`
+    },
+    {
+      name : '卓6',
+      imgUrl: `${DIR_IMAGE}/taku_logo.png`,
+      slide: "https://www.slideshare.net/slideshow/embed_code/key/rhIYkBYQFRWEKm",
+      disc : `卓6ですこんにちは</br>
+      こんにちは<b>こんにちは</b>
+      こんにちはこんにちは`
+    },
+    {
+      name : '卓7',
+      imgUrl: `${DIR_IMAGE}/taku_logo.png`,
+      slide: "https://www.slideshare.net/slideshow/embed_code/key/rhIYkBYQFRWEKm",
+      disc : `卓7ですこんにち</br>
+      こんにちは<b>こんにちは</b>
+      こんにちはこんにちは`
+    },
+    {
+      name : '卓9',
+      imgUrl: `${DIR_IMAGE}/taku_logo.png`,
+      slide: "https://www.slideshare.net/slideshow/embed_code/key/rhIYkBYQFRWEKm",
+      disc : `卓8ですこんにちは</br>
+      こんにちは<b>こんにちは</b>
+      こんにちはこんにちは`
+    },
+    {
+      name : '卓9',
+      imgUrl: `${DIR_IMAGE}/taku_logo.png`,
+      slide: "https://www.slideshare.net/slideshow/embed_code/key/rhIYkBYQFRWEKm",
+      disc : `卓9ですこんにち</br>
+      こんにちは<b>こんにちは</b>
+      こんにちはこんにちは`
+    },
+    {
+      name : '卓10',
+      imgUrl: `${DIR_IMAGE}/taku_logo.png`,
+      slide: "https://www.slideshare.net/slideshow/embed_code/key/rhIYkBYQFRWEKm",
+      disc : `卓10ですこんにち</br>
+      こんにちは<b>こんにちは</b>
+      こんにちはこんにちは`
+    },
+    {
+      name : '卓11',
+      imgUrl: `${DIR_IMAGE}/taku_logo.png`,
+      slide: "https://www.slideshare.net/slideshow/embed_code/key/rhIYkBYQFRWEKm",
+      disc : `卓11ですこんに</br>
+      こんにちは<b>こんにちは</b>
+      こんにちはこんにちは`
+    },
+    {
+      name : '卓12',
+      imgUrl: `${DIR_IMAGE}/taku_logo.png`,
+      slide: "https://www.slideshare.net/slideshow/embed_code/key/rhIYkBYQFRWEKm",
+      disc : `卓11ですこんに</br>
+      こんにちは<b>こんにちは</b>
+      こんにちはこんにちは`
+    },
+    {
+      name : '卓13',
+      imgUrl: `${DIR_IMAGE}/taku_logo.png`,
+      slide: "https://www.slideshare.net/slideshow/embed_code/key/rhIYkBYQFRWEKm",
+      disc : `卓11ですこん</br>
+      こんにちは<b>こんにちは</b>
+      こんにちはこんにちは`
+    }
   ];
 
   // ロゴをランダムに表示する
@@ -45,28 +128,32 @@ $(function(){
   for(var i = 0 ; i < group.length ; i++){
     var num = numArray[i];
     var imgUrl = group[num].imgUrl;
-    $('.logo-container').append(`<a href='#modal'><img class='remodal-btn' id='${num}' src='${imgUrl}'/></a>`);
+    $('.logo-container').append(`
+      <div class="column is-one-fifth-desktop is-half-mobile is-multiline">
+      <img class='remodal-btn' id='${num}' src='${imgUrl}'/>
+      </div>`
+    );
   }
 
    //モーダルの中身
-   $('.remodal-btn').on("click", function(){
-     $('.remodal').children(".remodal-contents").remove();
 
-     var name = group[$(this).attr('id')].name;
-     var img = group[$(this).attr('id')].imgUrl;
-     var slide = group[$(this).attr('id')].slide;
-     var url = group[$(this).attr('id')].url;
-     var disc = group[$(this).attr('id')].disc;
+  var $logo = $(".group-logo-container img");
+  var $disc = $(".group-discription");
+  var $slide = $(".group-slide iframe");
+  var name, img, slide, disc;
 
-     $('.remodal').append(
-       `<div class="remodal-contents">
-          <p>${name}</p>
-          <img src="${img}">
-          <iframe src="${slide}" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe> 
-          <p>${disc}</p>
-        </div>`
-     );
+  $('.remodal-btn').on("click", function(){
+    name = group[$(this).attr('id')].name;
+    img = group[$(this).attr('id')].imgUrl;
+    slide = group[$(this).attr('id')].slide;
+    disc = group[$(this).attr('id')].disc;
 
-     $('[data-remodal-id=modal]').remodal();
-   });
+    $slide.attr({src:`${slide}`});
+    $logo.attr({src:`${img}`,alt:`${name}のロゴ`});
+    $disc.html(`${disc}`);
+
+    setTimeout(function(){
+      $('[data-remodal-id=modal]').remodal().open();
+    },600);
+  });
 });
